f68bdcc0fee088b74a9ef81e38848c9fb63f6f25
Jeffrey506 <pjf506@163.com>
2013-11-12 02:17:45 +0000

    change_column_layout_to_table

--- app/controllers/columns_controller.rb
+++ app/controllers/columns_controller.rb
@@ -1,6 +1,6 @@
 class ColumnsController < ApplicationController
 
-  http_basic_authenticate_with name: "pjf", password: "pjfmodel", only: :destroy
+  http_basic_authenticate_with name: "pjf", password: "pjfmodel", only: [:create, :destroy]
 
   def create
     @table = Table.find(params[:table_id])
