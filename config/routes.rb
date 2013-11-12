2dba5a536b96e5239e9542f2528abf76fe466fe1
Jeffrey506 <pjf506@163.com>
2013-11-12 00:37:20 +0000

    Finish static pages & Tutorial advanced settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Model::Application.routes.draw do
-
   get "static_pages/home"
   get "static_pages/help"
+  get "static_pages/about"
+
   resources :tables do
     resources :columns
   end
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe "Static Pages" do
+
+  describe "Home page" do
+    before { visit '/static_pages/home' }
+    it "should have the content 'Model Manager'" do
+      expect(page).to have_content('Model Manager')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Model Manager | Home")
+    end
+  end
+
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the title 'Model Manager | Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Model Manager | Help")
+    end
+  end
+
+  describe "About page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Model Manager | About Us")
+    end
+  end
+end
