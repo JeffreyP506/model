cc5ee67c72e2972d0b7fa396753ac76de5dc37ef
Jeffrey506 <pjf506@163.com>
2013-11-15 01:41:52 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Model::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  get "users/new"
+  match '/signup', to: 'users#new', via: 'get'
+  match '/home', to: 'static_pages#home', via: 'get'
+  match '/help', to: 'static_pages#help', via: 'get'
+  match '/about', to: 'static_pages#about', via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
 
   resources :tables do
     resources :columns
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,43 +1,34 @@
 require 'spec_helper'
 
 describe "Static Pages" do
+  subject { page }
 
   describe "Home page" do
-    before { visit '/static_pages/home' }
-    it "should have the content 'Model Manager'" do
-      expect(page).to have_content('Model Manager')
-    end
+    before { visit home_path }
 
-    it "should have the base title" do
-      expect(page).to have_title("Model Manager")
-    end
-
-    it "should not have a custom page title" do
-      expect(page).not_to have_title("| Home")
-    end
+    it { should have_content('Model Manager') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('|Home') }
   end
 
   describe "Help page" do
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
+    before { visit help_path }
 
-    it "should have the title 'Model Manager | Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("Model Manager | Help")
-    end
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
+    before { visit about_path }
 
-    it "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("Model Manager | About Us")
-    end
+    it { should have_content('About Us') }
+    it { should have_title(full_title('About Us')) }
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 end
