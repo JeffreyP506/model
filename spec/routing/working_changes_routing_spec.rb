0e063909121752b8bcd9243144e59358dccc19b2
Jeffrey506 <pjf506@163.com>
2013-12-30 15:16:49 +0800

    fix routing spec     change .should to exptect().to

--- spec/routing/working_changes_routing_spec.rb
+++ spec/routing/working_changes_routing_spec.rb
@@ -4,31 +4,31 @@ describe WorkingChangesController do
   describe "routing" do
 
     it "routes to #index" do
-      get("/working_changes").should route_to("working_changes#index")
+      expect(get("/working_changes")).to route_to("working_changes#index")
     end
 
     it "routes to #new" do
-      get("/working_changes/new").should route_to("working_changes#new")
+      expect(get("/working_changes/new")).to route_to("working_changes#new")
     end
 
     it "routes to #show" do
-      get("/working_changes/1").should route_to("working_changes#show", :id => "1")
+      expect(get("/working_changes/1")).to route_to("working_changes#show", :id => "1")
     end
 
     it "routes to #edit" do
-      get("/working_changes/1/edit").should route_to("working_changes#edit", :id => "1")
+      expect(get("/working_changes/1/edit")).to route_to("working_changes#edit", :id => "1")
     end
 
     it "routes to #create" do
-      post("/working_changes").should route_to("working_changes#create")
+      expect(post("/working_changes")).to route_to("working_changes#create")
     end
 
     it "routes to #update" do
-      put("/working_changes/1").should route_to("working_changes#update", :id => "1")
+      expect(put("/working_changes/1")).to route_to("working_changes#update", :id => "1")
     end
 
     it "routes to #destroy" do
-      delete("/working_changes/1").should route_to("working_changes#destroy", :id => "1")
+      expect(delete("/working_changes/1")).to route_to("working_changes#destroy", :id => "1")
     end
 
   end
